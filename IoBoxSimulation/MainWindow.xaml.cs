af0b18d2f8162c03d2093f608b58947b9d337300
Tina.Liang <tina.liang@advantech.com.tw>
2021-02-20 10:25:01 +0800

    Update overall layout

--- IoBoxSimulation/MainWindow.xaml.cs
+++ IoBoxSimulation/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,55 @@ namespace IoBoxSimulation
         public MainWindow()
         {
             InitializeComponent();
+            InitializeState();
+        }
+
+        /// <summary>
+        /// Initialize default state
+        /// </summary>
+        private void InitializeState()
+        {
+            rb_do0_off.IsChecked = true;
+            rb_do1_off.IsChecked = true;
+            rb_do2_off.IsChecked = true;
+            rb_do3_off.IsChecked = true;
+        }
+
+        private void btn_action_Click(object sender, RoutedEventArgs e)
+        {
+            if (btn_action.Content?.ToString() == "open")
+            {
+                btn_action.Content = "close";
+            } 
+            else
+            {
+                btn_action.Content = "open";
+            }
+        }
+
+        private void input_port_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            try
+            {
+                // validate number only
+                Regex regex = new Regex("^[0-9]+$");
+                e.Handled = !regex.IsMatch(e.Text);
+
+                if (e.Handled == true) return;
+
+                // validate number range 1 - 65535
+                int min = 1;
+                int max = 65535;
+
+                int input = int.Parse(input_port.Text + e.Text);
+
+                e.Handled = !(input >= min && input <= max);
+            }
+            catch (Exception)
+            {
+                e.Handled = true;
+            }
+
         }
     }
 }
